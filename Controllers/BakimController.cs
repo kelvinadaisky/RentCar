7b478f1908466c831dcc5882feaa3c92e5329b40
kelvinadaisky <kelvinadaiskyirutingabo2002@gmail.com>
2024-12-13 13:16:58 +0300

    implemented the Bakim

---
+++ Controllers/BakimController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentCar.Models;
+
+namespace RentCar.Controllers
+{
+    public class BakimController : Controller
+    {
+        private readonly RentCarContext _context;
+
+        public BakimController(RentCarContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bakim/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var bakim = _context.Bakims
+                .Include(b => b.IdArabaNavigation) // Include related car data
+                .FirstOrDefault(b => b.IdBakim == id);
+
+            if (bakim == null)
+            {
+                return NotFound();
+            }
+
+            return View(bakim);
+        }
+
+        // POST: Bakim/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Bakim bakim)
+        {
+            if (id != bakim.IdBakim)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bakim);
+                    _context.SaveChanges();
+                    TempData["success"] = "Maintenance details updated successfully!";
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Bakims.Any(b => b.IdBakim == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction("Details", "Arac", new { id = bakim.IdAraba });
+            }
+
+            return View(bakim);
+        }
+    }
+}
